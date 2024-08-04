d30c10b19cc3b847a707b4024db3b1736bf208b2
jhonlima97 <jwlimita08.997@gmail.com>
2024-08-04 03:17:10 -0500

    add more endpoints

--- src/server.js
+++ src/server.js
@@ -4,9 +4,17 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// Endpoint
+// Endpoints
 app.get('/api/explore', (req, res) => {
   res.sendFile(path.join(__dirname, 'assets/data/explore.json'));
+});
+
+app.get('/api/reviews', (req, res) => {
+  res.sendFile(path.join(__dirname, 'assets/data/reviews.json'));
+});
+
+app.get('/api/articles', (req, res) => {
+  res.sendFile(path.join(__dirname, 'assets/data/articles.json'));
 });
 
 app.listen(PORT, () => {
