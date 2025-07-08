3655771af2b3aed81b889c3e89336aa43422e3fe
jhonlima97 <jhonw.lima08@gmail.com>
2025-07-08 11:11:26 -0500

    fix

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,14 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideZonelessChangeDetection } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes), 
+    provideRouter(routes),
+    provideZonelessChangeDetection(), 
     provideClientHydration(),
     provideHttpClient(withFetch())]
 };
